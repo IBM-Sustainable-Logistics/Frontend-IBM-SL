77518a16c8eae294c24f128052ddd036c5ab495d
hananinas <hanani.nas@gmail.com>
2024-02-22 13:00:34 +0100

    Add Icons component and update index route

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,6 +1,8 @@
 import * as React from "react";
 import type { MetaFunction } from "@remix-run/deno";
 import { Button } from "../components/ui/button.tsx";
+import { GitHub } from "../lib/Icons.tsx";
+import { Link } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -13,7 +15,9 @@ export default function Index() {
   return (
     <div className=" flex flex-col justify-center items-center h-screen gap-6">
       <h1 className=" text-4xl font-bold">Coming Soon</h1>
-      <Button>View on Github</Button>
+      <Link to="https://github.com/IBM-Sustainable-Logistics">
+        <Button className="flex gap-2">{GitHub()} View on Github</Button>{" "}
+      </Link>
     </div>
   );
 }
