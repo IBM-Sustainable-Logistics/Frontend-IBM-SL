0a353ac231fae2f7c8ce0eee4c2a85da30b13a29
hananinas <hanani.nas@gmail.com>
2024-02-28 23:33:21 +0100

    Add input field and display success message after calculation

--- app/components/calculator.tsx
+++ app/components/calculator.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Button } from "../components/ui/button.tsx";
 import { Label } from "./ui/label.tsx";
 import { Combobox } from "./ui/combobox.tsx";
+import { Input } from "./ui/input.tsx";
 
 interface FormState {
   transportMethod: string;
@@ -23,11 +24,16 @@ const Calculator = () => {
   };
 
   const [formData, setFormData] = useState<FormState>(initialFormState);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [message, setMessage] = useState("");
+  const [showMessage, setShowMessage] = useState(false);
+  const [showError, setShowError] = useState(false);
 
   const handleInputChange = (
     e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
   ) => {
     const { name, value } = e.target;
+    console.log(name, value);
 
     // Update the corresponding property in the form data
     setFormData({
@@ -45,14 +51,25 @@ const Calculator = () => {
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-
+    console.log(formData);
     // We just log the values for now
-    console.log(
+    setMessage(
       `Calculating emissions for ${formData.distance} km using ${formData.transportMethod}`
     );
+    setShowMessage(true);
+    setShowError(false);
     // This is a placeholder for emissions calculation
     const calculatedEmissions = formData.distance * 0.2;
+
     formData.emissions = calculatedEmissions;
+
+    setMessage(
+      `Emissions calculated successfully which was ${formData.emissions}`
+    );
+
+    setTimeout(() => {
+      setShowMessage(false);
+    }, 3000);
   };
 
   return (
@@ -70,17 +87,29 @@ const Calculator = () => {
               type="transportMethod"
             />
           </div>
-
-          <label htmlFor="distance">Distance (km):</label>
-          <input
+          <Label className="text-lg font-medium text-gray-900 dark:text-gray-100">
+            Distance (km):
+          </Label>
+          <Input
             type="number"
             id="distance"
+            name="distance"
             className="w-full px-4 py-3 border-2 placeholder:text-gray-800 rounded-md outline-none focus:ring-4 border-gray-300 focus:border-gray-600 ring-gray-100"
             onChange={handleInputChange}
           />
           <Button className="w-full" variant={"ibm_blue"} type="submit">
             Calculate
           </Button>
+          {showMessage && (
+            <div className="bg-green-200 p-3 mb-3 rounded-md text-green-800">
+              {message}
+            </div>
+          )}
+          {showError && (
+            <div className="bg-red-200 p-3 mb-3 rounded-md text-red-800 mt-6">
+              {errorMessage}
+            </div>
+          )}
         </div>
       </form>
     </div>
