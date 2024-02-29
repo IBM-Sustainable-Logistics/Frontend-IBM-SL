5ed25a1bfd562ed716d960bcb50a629fba6fe6d2
Tan Dang <tandang2002@live.dk>
2024-02-29 18:16:17 +0100

    tried to request data from the backend
    IT DOES NOT WORK D:

--- app/components/calculator.tsx
+++ app/components/calculator.tsx
@@ -5,114 +5,133 @@ import { Combobox } from "./ui/combobox.tsx";
 import { Input } from "./ui/input.tsx";
 
 interface FormState {
-  transportMethod: string;
-  distance: number;
-  emissions: number | null;
+    transport_form: string;
+    distance_km: number;
 }
 
 const Calculator = () => {
-  const transportMethod = [
-    { value: "container-vessel", label: "Container Vessel" },
-    { value: "road", label: "Road" },
-    { value: "rail", label: "Rail" },
-    { value: "air", label: "Air" },
-  ];
+    const transportMethod = [
+        { value: "container-vessel", label: "Container Vessel" },
+        { value: "road", label: "Road" },
+        { value: "rail", label: "Rail" },
+        { value: "air", label: "Air" },
+    ];
 
-  const initialFormState: FormState = {
-    transportMethod: "car",
-    distance: 0,
-    emissions: null,
-  };
+    const initialFormState: FormState = {
+        transport_form: "car",
+        distance_km: 0,
+    };
 
-  const [formData, setFormData] = useState<FormState>(initialFormState);
-  const [errorMessage, setErrorMessage] = useState("");
-  const [message, setMessage] = useState("");
-  const [showMessage, setShowMessage] = useState(false);
-  const [showError, setShowError] = useState(false);
+    const [formData, setFormData] = useState<FormState>(initialFormState);
+    const [errorMessage, setErrorMessage] = useState("");
+    const [message, setMessage] = useState("");
+    const [showMessage, setShowMessage] = useState(false);
+    const [showError, setShowError] = useState(false);
 
-  const handleInputChange = (
-    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-  ) => {
-    const { name, value } = e.target;
-    console.log(name, value);
+    const handleInputChange = (
+        e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+    ) => {
+        const { name, value } = e.target;
+        console.log(name, value);
 
-    // Update the corresponding property in the form data
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-  };
+        // Update the corresponding property in the form data
+    };
 
-  const handleSelectChange = (name: string, value: string) => {
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-  };
+    const handleSelectChange = (name: string, value: string) => {
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+    };
 
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    
-    try {
-      const response = await fetch("http://localhost:8000", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formData),
-      });
+    const handleSubmit = async () => {
+        // e.preventDefault();
 
-      if (!response.ok) {
-        throw new Error("Error! Got response: " + response.status);
-      }
+        const requestData = [
+            {
+                transport_form: "truck",
+                distance_km: 0,
+            },
+            {
+                transport_form: "boat",
+                distance_km: 0,
+            },
+            {
+                transport_form: "plane",
+                distance_km: 0,
+            },
+        ];
+        try {
+            const response = await fetch(
+                "https://ibm-sl-api.deno.dev/",
+                {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(requestData),
+                }
+            );
 
-      const responseData = await response.json();
-      setFormData({ ...formData, emissions: responseData.emissions });
-    } catch (error) {
-      console.error("Error:", error);
-    }
-  };
+            if (!response.ok) {
+                throw new Error("Error! Got response: " + response.status);
+            }
 
-  return (
-    <div className="flex flex-col gap-4">
-      <h1 className=" text-primary text-4xl font-bold">Calculate Emissions</h1>
-      <form onSubmit={handleSubmit}>
-        <div className=" flex flex-col gap-4 ">
-          <Label className="text-lg font-medium text-gray-900 dark:text-gray-100">
-            Transport Method:
-          </Label>
-          <Combobox
-            options={transportMethod}
-            onChangeTransport={handleSelectChange}
-            type="transportMethod"
-          />
-          <Label className="text-lg font-medium text-gray-900 dark:text-gray-100">
-            Distance (km):
-          </Label>
-          <Input
-            type="number"
-            id="distance"
-            name="distance"
-            className="w-full px-4 py-3 border-2 placeholder:text-gray-800 rounded-md outline-none focus:ring-4 border-gray-300 focus:border-gray-600 ring-gray-100"
-            onChange={handleInputChange}
-          />
-          <Button className="w-full" variant={"ibm_blue"} type="submit">
-            Calculate
-          </Button>
+            const responseData = await response.json();
+            console.log("Response:", responseData);
+        } catch (error) {
+            console.error("Error:", error.message);
+        }
+    };
+
+    return (
+        <div className="flex flex-col gap-4">
+            <h1 className=" text-primary text-4xl font-bold">
+                Calculate Emissions
+            </h1>
+            <form onSubmit={handleSubmit}>
+                <div className=" flex flex-col gap-4 ">
+                    <Label className="text-lg font-medium text-gray-900 dark:text-gray-100">
+                        Transport Method:
+                    </Label>
+                    <Combobox
+                        options={transportMethod}
+                        onChangeTransport={handleSelectChange}
+                        type="transport_form"
+                    />
+                    <Label className="text-lg font-medium text-gray-900 dark:text-gray-100">
+                        Distance (km):
+                    </Label>
+                    <Input
+                        type="number"
+                        id="distance"
+                        name="distance_km"
+                        className="w-full px-4 py-3 border-2 placeholder:text-gray-800 rounded-md outline-none focus:ring-4 border-gray-300 focus:border-gray-600 ring-gray-100"
+                        onChange={handleInputChange}
+                    />
+                    <Button
+                        className="w-full"
+                        type="submit"
+                        onClick={() => {
+                            handleSubmit();
+                        }}
+                    >
+                        Calculate
+                    </Button>
+                </div>
+            </form>
+            {showMessage && (
+                <div className="bg-green-200 p-3 mb-3 rounded-md text-green-800 w-[330px]">
+                    {message}
+                </div>
+            )}
+            {showError && (
+                <div className="bg-red-200 p-3 mb-3 rounded-md text-red-800 mt-6 w-[330px]">
+                    {errorMessage}
+                </div>
+            )}
         </div>
-      </form>
-      {showMessage && (
-        <div className="bg-green-200 p-3 mb-3 rounded-md text-green-800 w-[330px]">
-          {message}
-        </div>
-      )}
-      {showError && (
-        <div className="bg-red-200 p-3 mb-3 rounded-md text-red-800 mt-6 w-[330px]">
-          {errorMessage}
-        </div>
-      )}
-    </div>
-  );
+    );
 };
 
 export default Calculator;
