1e545187ca901452b2091c809ae884dcdea82215
hananinas <hanani.nas@gmail.com>
2024-03-01 00:16:50 +0100

    Refactor hero and navbar components

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -1,20 +1,28 @@
 import React from "react";
 import { TextGenerateEffect } from "../components/ui/text-generate-effect.tsx";
+import { Button } from "../components/ui/button.tsx";
+import { GitHub } from "../lib/Icons.tsx";
+import { Link } from "@remix-run/react";
 
 export default function Hero() {
-    const message = "Empowering the future of sustainable logistics with IBM";
-    return (
-        <section id="hero">
-            <div className="w-screen flex">
-                <div className="">
-                    <div className="w-screen flex flex-col items-center justify-center h-screen">
-                        <TextGenerateEffect
-                            words={message}
-                            className="text-center text-2xl lg:text-4xl"
-                        />
-                    </div>
-                </div>
-            </div>
-        </section>
-    );
+  const message = "Empowering the future of sustainable logistics with IBM";
+  return (
+    <section id="hero">
+      <div className="w-screen flex">
+        <div className="">
+          <div className="w-screen flex flex-col items-center justify-center h-screen gap-10">
+            <TextGenerateEffect
+              words={message}
+              className="text-center text-2xl lg:text-4xl"
+            />
+            <Link to="https://github.com/IBM-Sustainable-Logistics">
+              <Button variant={"ibm_blue"} className="flex gap-2">
+                {GitHub()} View on Github
+              </Button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,8 +1,6 @@
 import * as React from "react";
 import type { MetaFunction } from "@remix-run/deno";
-import { Button } from "../components/ui/button.tsx";
-import { GitHub } from "../lib/Icons.tsx";
-import { Link } from "@remix-run/react";
+
 import Hero from "../components/hero.tsx";
 
 export const meta: MetaFunction = () => {
@@ -16,11 +14,6 @@ export default function Index() {
   return (
     <div className="flex flex-col justify-center items-center h-screen gap-6 bg-background font-mono">
       <Hero /> {/* Use the Hero component */}
-      <Link to="https://github.com/IBM-Sustainable-Logistics">
-        <Button variant={"ibm_blue"} className="flex gap-2">
-          {GitHub()} View on Github
-        </Button>
-      </Link>
     </div>
   );
 }
