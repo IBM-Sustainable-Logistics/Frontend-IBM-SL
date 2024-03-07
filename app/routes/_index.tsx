4234a566ab81b264d07bbb76daf8cf6cc6a73fcd
hananinas <hanani.nas@gmail.com>
2024-03-07 13:45:57 +0100

    Added supabase ssr

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
-import type { MetaFunction } from "@remix-run/deno";
+import type { MetaFunction, LoaderFunctionArgs } from "@remix-run/deno";
+import { createServerClient, parse, serialize } from 'https://esm.sh/@supabase/ssr@0.1.0'
+import { load } from "https://deno.land/std@0.218.0/dotenv/mod.ts";
 
 import Hero from "../components/hero.tsx";
 
@@ -9,6 +11,32 @@ export const meta: MetaFunction = () => {
     { name: "description", content: "Welcome to SL" },
   ];
 };
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  const cookies = parse(request.headers.get('Cookie') ?? '')
+  const headers = new Headers()
+
+  const env = await load();
+
+
+  const supabase = createServerClient(env.SUPABASE_URL!, env.SUPABASE_ANON_KEY!, {
+    cookies: {
+      get(key) {
+        return cookies[key]
+      },
+      set(key, value, options) {
+        headers.append('Set-Cookie', serialize(key, value, options))
+      },
+      remove(key, options) {
+        headers.append('Set-Cookie', serialize(key, '', options))
+      },
+    },
+  })
+
+  return new Response('...', {
+    headers,
+  })
+}
 
 export default function Index() {
   return (
