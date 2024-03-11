1b4e571f50a61eabbc4ca148c962e26fa9ccfa2c
Tan Dang <tandang2002@live.dk>
2024-03-11 15:11:12 +0100

    added the edit icon back
    title

--- app/components/ProjectCard.tsx
+++ app/components/ProjectCard.tsx
@@ -62,7 +62,21 @@ const ProjectCard = ({
                             className="border-black border rounded"
                         >
                             Edit
-                            {/* SVG icon for edit functionality */}
+                            <svg
+                                className="w-6 h-6 text-gray-800 dark:text-white"
+                                aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                            >
+                                <path
+                                    stroke="currentColor"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="m10.8 17.8-6.4 2.1 2.1-6.4m4.3 4.3L19 9a3 3 0 0 0-4-4l-8.4 8.6m4.3 4.3-4.3-4.3m2.1 2.1L15 9.1m-2.1-2 4.2 4.2"
+                                />
+                            </svg>
                         </Button>
                     </DialogTrigger>
                     <DialogContent>
