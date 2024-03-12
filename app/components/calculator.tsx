07be0761f7b98bb8533df497baa06c6fc58c0afc
daah <daah@itu.dk>
2024-03-12 17:19:47 +0100

    resizing

--- app/components/calculator.tsx
+++ app/components/calculator.tsx
@@ -96,8 +96,8 @@ const Calculator = () => {
   };
 //<div className=" min-h-screen flex items-center justify-center">
   return (
-    <div className="flex flex-row ">
-      <div className="basis-2/3 flex justify-center">
+    <div className="flex flex-row h-screen">
+      <div className="basis-2/3 flex justify-center items-center">
         <div className="flex flex-col gap-4 ">
           <h1 className=" text-primary text-4xl font-bold">Calculate Emissions</h1>
           <form onSubmit={handleSubmit}>
@@ -137,13 +137,16 @@ const Calculator = () => {
           )}
         </div>
       </div>
-      <ScrollArea className="h-[200px] w-[350px] rounded-md border p-4 basis-1/3">
-        Jokester began sneaking into the castle in the middle of the night and leaving
-        jokes all over the place: under the king's pillow, in his soup, even in the
-        royal toilet. The king was furious, but he couldn't seem to stop Jokester. And
-        then, one day, the people of the kingdom discovered that the jokes left by
-        Jokester were so funny that they couldn't help but laugh. And once they
-        started laughing, they couldn't stop.
+      <ScrollArea className="h-[200px] w-[350px] rounded-md border p-4 basis-1/3 h-screen">
+        <div className="border border-black p-5 my-5">
+          test
+        </div>
+        <div className="border border-black p-5 my-5">
+          test
+        </div>
+        <div className="border border-black p-5 my-5">
+          test
+        </div>
       </ScrollArea>
     </div>
   );
