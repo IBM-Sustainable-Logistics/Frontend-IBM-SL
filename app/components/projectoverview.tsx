bdc389d94df8904be58d6602a2a8ebaa629dd5d1
Burak Özdemir <75176220+Burak123T@users.noreply.github.com>
2024-03-13 15:20:32 +0100

    added accordion

--- app/components/projectoverview.tsx
+++ app/components/projectoverview.tsx
@@ -1,31 +1,38 @@
-import React, { useEffect, useState } from "react";
-import { Button } from "./ui/button.tsx";
-import { ProjectCard } from "./ProjectCard.tsx";
-import { Input } from "./ui/input.tsx";
 import {
-    Pagination,
-    PaginationContent,
-    PaginationEllipsis,
-    PaginationItem,
-    PaginationLink,
-    PaginationNext,
-    PaginationPrevious,
-} from "./ui/pagination";
-
-import {
-    Dialog,
-    DialogClose,
-    DialogContent,
-    DialogDescription,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
-} from "./ui/dialog.tsx";
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+} from './ui/accordion'
 
 const ProjectOverview = () => {
     return (
         <>
-            <h1>Here goes the project details...</h1>
+            <div>
+                return (
+                <Accordion type="single" collapsible className="w-full">
+                    <AccordionItem value="item-1">
+                        <AccordionTrigger>Is it accessible?</AccordionTrigger>
+                        <AccordionContent>
+                            Yes. It adheres to the WAI-ARIA design pattern.
+                        </AccordionContent>
+                    </AccordionItem>
+                    <AccordionItem value="item-2">
+                        <AccordionTrigger>Is it styled?</AccordionTrigger>
+                        <AccordionContent>
+                            Yes. It comes with default styles that matches the other
+                            components&apos; aesthetic.
+                        </AccordionContent>
+                    </AccordionItem>
+                    <AccordionItem value="item-3">
+                        <AccordionTrigger>Is it animated?</AccordionTrigger>
+                        <AccordionContent>
+                            Yes. It's animated by default, but you can disable it if you prefer.
+                        </AccordionContent>
+                    </AccordionItem>
+                </Accordion>
+                )
+            </div>
         </>
     );
 };
