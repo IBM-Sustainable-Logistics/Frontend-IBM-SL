59c6b647808c4d5241bcf495b7cc9d258d365f3b
Burak123T <burak-9@live.dk>
2024-03-13 11:03:34 +0100

    Added structure for dynamic routing

---
+++ app/routes/projects.$title.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Dashboard from "../components/dashboard.js";
+
+const ProjectById = () => {
+
+    return (
+        <div className="max-h-lvh">
+            <Dashboard />
+        </div>
+    );
+};
+
+export default ProjectById;
