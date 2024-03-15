898af5db899855a70f1c92f62453b5768b5ba104
Ch Hanan Hadayat <hanani.nas@gmail.com>
2024-03-15 12:37:03 +0100

    added height

--- app/routes/data-visualization.tsx
+++ app/routes/data-visualization.tsx
@@ -5,20 +5,19 @@ import { getSupabaseWithSessionAndHeaders } from "../lib/supabase-server.ts";
 import { LoaderFunctionArgs, json } from "@remix-run/deno";
 
 export let loader = async ({ request }: LoaderFunctionArgs) => {
-    const { headers, serverSession } = await getSupabaseWithSessionAndHeaders({
-      request,
-    });
+  const { headers, serverSession } = await getSupabaseWithSessionAndHeaders({
+    request,
+  });
 
-  
-    return json({ success: true }, { headers });
-  };
+  return json({ success: true }, { headers });
+};
 
 const DVPage = () => {
-    return (
-        <div className="max-h-lvh"> 
-            <DataVisualization />
-        </div>
-    );
+  return (
+    <div className="max-h-lvh min-h-screen ">
+      <DataVisualization />
+    </div>
+  );
 };
 
 export default DVPage;
