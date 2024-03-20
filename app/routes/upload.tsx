633a863bd83a841a61d56b377a03cd9c8dd4daec
Burak123T <buoe@itu.dk>
2024-03-20 22:36:51 +0100

    show filename being dropped

--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -14,6 +14,10 @@ import { useState } from "react"
 
 const UploadFile = () => {
 
+  const [hasUploaded, setHasUploaded] = useState(false);
+  const [onHover, setOnHover] = useState(false);
+  const [fileName, setFileName] = useState<string | null>(null);
+
   /**
    * Code template taken from: https://developer.mozilla.org/en-US/docs/Web/API/HTML_Drag_and_Drop_API/File_drag_and_drop 
    */
@@ -30,10 +34,13 @@ const UploadFile = () => {
         if (item.kind === "file") {
           const file = item.getAsFile();
           if (file) {
+            setFileName(file.name);
             if (file.name.endsWith(".csv")) {
               console.log(`… file[${i}].name = ${file.name}`);
+              setHasUploaded(true);
             } else {
               console.log(`file rejected: ${file.name}`)
+              setHasUploaded(false);
             }
           }
 
@@ -47,14 +54,12 @@ const UploadFile = () => {
     }
   }
 
-  const [hasUploaded, setHasUploaded] = useState(false);
 
   function handleUploadClick(): void {
     const fileInput = document.getElementById("fileInput");
     if (fileInput) {
       fileInput.click();
     }
-    setHasUploaded(!hasUploaded);
   }
 
   function deleteUpload(): void {
@@ -64,13 +69,25 @@ const UploadFile = () => {
   function dragOverHandler(ev: React.DragEvent<HTMLDivElement>): void {
     console.log("File(s) in drop zone");
 
-    // Prevent default behavior (Prevent file from being opened)
+    setOnHover(true);
+
+    ev.preventDefault();
+  }
+
+  function dragHoverEnd(ev: React.DragEvent<HTMLDivElement>): void {
+    setOnHover(false);
+
     ev.preventDefault();
   }
 
   return (
-    <div id="drop_zone" onDrop={dropHandler} onDragOver={dragOverHandler}>
-      <Card className="w-[350px]">
+    <div id="drop_zone" onDrop={dropHandler} onDragOver={dragOverHandler} onDragLeave={dragHoverEnd}>
+      <Card
+        className="w-[350px]"
+        style={{
+          backgroundColor: onHover ? "lightgray" : "white",
+        }}
+      >
         <CardHeader>
           <CardTitle>Upload your file here</CardTitle>
           <CardDescription>You can drag and drop your file here. Alternatively, you can click the 'Choose file' button below: </CardDescription>
@@ -79,7 +96,11 @@ const UploadFile = () => {
           <form>
             <div className="grid w-full items-center gap-4">
               <div className="flex flex-col space-y-1.5">
-                <Label htmlFor="name">File name:</Label>
+                {fileName === null ? (
+                  <Label htmlFor="name">File name:</Label>
+                ) : (
+                  <Label htmlFor="name">File name: {fileName}</Label>
+                )}
                 <input type="file" id="fileInput" style={{ display: "none" }} />
               </div>
             </div>
