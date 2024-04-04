95c8adcc8b4dcf53e533c35c24e117acadf342cd
hananinas <hanani.nas@gmail.com>
2024-04-04 13:22:03 +0200

    Fix ui

--- app/components/dashboard/dashboard.tsx
+++ app/components/dashboard/dashboard.tsx
@@ -139,26 +139,22 @@ const Dashboard: React.FC<DashboardProps> = ({
                     <Button className="w-full" onClick={addCalculator}>
                       Add transport method
                     </Button>
+                    <Button
+                      className="border-black border rounded"
+                      variant="default"
+                      onClick={handleCreateProject}
+                    >
+                      Create
+                    </Button>
+                    <Button
+                      className="border-black border rounded"
+                      variant="destructive"
+                    >
+                      Cancel
+                    </Button>
                   </div>
                 </DialogDescription>
               </DialogHeader>
-              <DialogClose asChild>
-                <Button
-                  className="border-black border rounded"
-                  variant="default"
-                  onClick={handleCreateProject}
-                >
-                  Create
-                </Button>
-              </DialogClose>
-              <DialogClose asChild>
-                <Button
-                  className="border-black border rounded"
-                  variant="destructive"
-                >
-                  Cancel
-                </Button>
-              </DialogClose>
             </DialogContent>
           </Dialog>
         </div>
