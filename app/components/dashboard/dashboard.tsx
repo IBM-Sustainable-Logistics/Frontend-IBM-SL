7d2e27da81044f13183e02dbacef1103972aa451
hananinas <hanani.nas@gmail.com>
2024-04-09 10:32:34 +0200

    Refactor ui for mobile plus refactor dashboard structure

--- app/components/dashboard/dashboard.tsx
+++ app/components/dashboard/dashboard.tsx
@@ -11,19 +11,8 @@ import {
   PaginationNext,
   PaginationPrevious,
 } from "../ui/pagination.tsx";
-import { CalculatorInstance, project } from "../../lib/Transport.ts";
-import {
-  Dialog,
-  DialogClose,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "../ui/dialog.tsx";
-import Calculator, { FormData, defaultFormData } from "../calculator.tsx";
-import { useFetcher } from "@remix-run/react";
-import { PlusIcon } from "../../lib/Icons.tsx";
+import { project } from "../../lib/Transport.ts";
+import CreateProject from "./dialogs/createproject.tsx";
 
 interface DashboardProps {
   Projects: project[];
@@ -34,41 +23,6 @@ const Dashboard: React.FC<DashboardProps> = ({
   Projects,
   UserId,
 }: DashboardProps) => {
-
-  // State to keep track of the number of Calculator components
-  const [calculators, setCalculators] = useState<CalculatorInstance[]>([]);
-
-  const [formData, setFormData] = useState<FormData>(defaultFormData());
-
-  const [titleProject, setTitleProject] = useState("");
-  const [descriptionProject, setDescriptionProject] = useState("");
-  const fetcher = useFetcher();
-
-  const addCalculator = () => {
-    const newCalculator = {
-      id: Date.now(), // Using the current timestamp as a unique ID
-    };
-    setCalculators([...calculators, newCalculator]);
-  };
-  const deleteCalculator = (id: number) => {
-    setCalculators(
-      calculators.filter(
-        (calculator: CalculatorInstance) => calculator.id !== id
-      )
-    );
-  };
-
-  const handleCreateProject = () => {
-    // after calculating the emissions, we can submit the form
-    const project = {
-      title: titleProject,
-      descriptionProject: descriptionProject,
-      userId: UserId,
-      calc: JSON.stringify(formData),
-    };
-    fetcher.submit(project, { method: "POST", action: "/api/project" });
-  };
-
   return (
     <>
       <h1 className="text-3xl font-bold my-2 text-center">My Projects</h1>
@@ -79,79 +33,10 @@ const Dashboard: React.FC<DashboardProps> = ({
             placeholder="Search for a project"
             className="w-full"
           />
-          <Dialog>
-            <DialogTrigger>
-              <Button
-                variant="outline"
-                className="flex items-center justify-between"
-              >
-                <span className="mr-2">Create a project</span>
-                <PlusIcon />
-              </Button>
-            </DialogTrigger>
-            <DialogContent className="h-2/3 overflow-y-auto">
-              <DialogHeader>
-                <DialogTitle>Create a project</DialogTitle>
-                <DialogDescription>
-                  <div
-                    className="flex flex-col gap-4"
-                    style={{ maxHeight: "90vh" }}
-                  >
-                    <Input
-                      type="text"
-                      placeholder="Title"
-                      className="w-full"
-                      onChange={(e) => setTitleProject(e.target.value)}
-                    />
-                    <Input
-                      type="text"
-                      placeholder="Description"
-                      className="w-full"
-                      onChange={(e) => setDescriptionProject(e.target.value)}
-                    />
-                    {calculators.map((calculator: CalculatorInstance) => (
-                      <div key={calculator.id} className=" w-full">
-                        <Calculator
-                          isCreateProject={true}
-                          formData={formData}
-                          setFormData={setFormData}
-                        />
-                        <Button
-                          variant="destructive"
-                          onClick={() => deleteCalculator(calculator.id)}
-                        >
-                          Delete
-                        </Button>
-                      </div>
-                    ))}
-                    <Button className="w-full" onClick={addCalculator}>
-                      Add transport method
-                    </Button>
-                    <DialogClose asChild>
-                      <Button
-                        className="border-black border rounded"
-                        variant="default"
-                        onClick={handleCreateProject}
-                      >
-                        Create
-                      </Button>
-                    </DialogClose>
-                    <DialogClose asChild>
-                      <Button
-                        className="border-black border rounded"
-                        variant="destructive"
-                      >
-                        Cancel
-                      </Button>
-                    </DialogClose>
-                  </div>
-                </DialogDescription>
-              </DialogHeader>
-            </DialogContent>
-          </Dialog>
+          <CreateProject UserId={UserId} />
         </div>
 
-        <div className="grid grid-cols-3 justify-self-stretch max-w-full gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-3 justify-self-stretch max-w-full gap-4">
           {Projects.map((p, index) => {
             return (
               <ProjectCard
---
+++ tests/indext.test.tsx
@@ -0,0 +1,10 @@
+import { json } from "@remix-run/deno";
+import { useLoaderData } from "@remix-run/react";
+import { createRemixStub } from "@remix-run/testing";
+import { render, screen, waitFor } from "@testing-library/react";
+import { assert } from "https://deno.land/std@0.128.0/_util/assert.ts";
+import React from "https://esm.sh/react@18.2.0";
+
+Deno.test("renders loader data", async () => {
+  
+});
