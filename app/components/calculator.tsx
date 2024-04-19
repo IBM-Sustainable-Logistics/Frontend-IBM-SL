733c7fc3ebdf56c37be609623c280eb8466b6a90
marc9622 <maaw@itu.dk>
2024-04-19 13:31:10 +0200

    Suggestion uses /fuzzy instead of /suggest

--- app/components/calculator.tsx
+++ app/components/calculator.tsx
@@ -204,7 +204,7 @@ const Calculator = ({
         country: address.country,
       };
 
-      const response = await fetch("/api/suggest", {
+      const response = await fetch("/api/fuzzy", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(input),
