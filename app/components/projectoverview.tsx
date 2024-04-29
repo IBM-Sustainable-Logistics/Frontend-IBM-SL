3590855c9e0e52c3944d96f9f9d6c767028c67c2
hananinas <hanani.nas@gmail.com>
2024-04-29 16:06:47 +0200

    change to right

--- app/components/projectoverview.tsx
+++ app/components/projectoverview.tsx
@@ -92,7 +92,6 @@ const ProjectOverview: React.FC<Props> = ({ project }) => {
       extraLength: 3, // A bigger number means that columns will be wider
       writeMode: "writeFile", // The available parameters are 'WriteFile' and 'write'. This setting is optional. Useful in such cases https://docs.sheetjs.com/docs/solutions/output#example-remote-file
       writeOptions: {}, // Style options from https://docs.sheetjs.com/docs/api/write-options
-      RTL: true, // Display the columns from right-to-left (the default value is false)
     };
 
     let data = project.chain.routes.map((route) => {
