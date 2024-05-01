124a4e8ecc9e52d4b955c4f6798cc717dbc1e1a9
Joshua <jcal@itu.dk>
2024-05-01 15:00:15 +0200

    Adjust the hero section in the front page for larger screen

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -4,16 +4,15 @@ import { Link } from "@remix-run/react";
 import heroImage from "../assets/undraw_calculation.svg";
 
 export default function Hero() {
-  const message = "Empowering the future of sustainable logistics with IBM";
   return (
     <section id='hero' className='w-auto ml-5rem lg:ml-[2rem] pb-10	lg:p-0'>
       <div className='w-full px-2 flex flex-col-reverse gap-10 lg:flex-row'>
         <div className='max-w-[40rem] flex flex-col gap-6'>
-          <h1 className='text-4xl lg:text-[3vw] leading-[3rem] font-bold'>
+          <h1 className='text-4xl lg:text-[3vw] leading-[3rem] font-bold min-[1800px]:leading-[4.25rem]'>
             Calculate your footprint emissions for
             <span style={{color: "#3d815c"}}> FREE</span>
           </h1>
-          <p className='text-2xl lg:text-[1.5vw]'>
+          <p className='text-2xl lg:text-[1.5vw] min-[1800px]:leading-[3.5rem]'>
             Use our web service to calculate your carbon footprint from the
             transportation of your truck and e-truck.
           </p>
