7b968d16efb8b089eb71da1d859fa89bfcc35225
hananinas <hanani.nas@gmail.com>
2024-05-11 18:16:23 +0200

    Update server.ts and $.tsx files

--- app/routes/$.tsx
+++ app/routes/$.tsx
@@ -4,6 +4,7 @@ import { SupabaseOutletContext } from "../lib/supabase.ts";
 import Hero from "../components/hero.tsx";
 import { Link } from "@remix-run/react";
 import { Button } from "../components/ui/button.tsx";
+import truck from "../assets/dump-truck.gif";
 
 const notfound = () => {
   const { supabase, domainUrl } = useOutletContext<SupabaseOutletContext>();
@@ -14,16 +15,15 @@ const notfound = () => {
       md:mt-20
       md:gap-32'
     >
-      <Hero
-        jsx={(<>
+      
           <h1 className='text-4xl lg:text-[3vw] leading-[3rem] font-bold min-[1800px]:leading-[4.25rem]'>
             Whoops, 404!
           </h1>
-          <p className='text-2xl lg:text-[1.5vw] min-[1800px]:leading-[3.5rem]'>
-            We could not find the page that you are looking for.
-          </p>
-        </>)}
-      />
+          <img
+            src={truck}
+            alt='Page not found'
+            className='w-full'/>
+
     </main>
   );
 };
